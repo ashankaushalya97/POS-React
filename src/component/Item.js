71ccaccc6705c6f8ae5b5146f148085dec202a5d
Kaweesha De Alwis <kaweesha12lk@gmail.com>
2020-02-20 16:05:10 +0530

    update item file

--- src/component/Item.js
+++ src/component/Item.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import 'antd/dist/antd.css';
+import { Form, Select, Input, Button } from 'antd';
 
-export default function Item() {
+
+export default function Item(props) {
+    const { Option } = Select;
+    const { getFieldDecorator } = props.form;
+
     return (
+            
         <div>
-            itemmmmmmmm
+        <Form labelCol={{ span: 5 }} wrapperCol={{ span: 12 }} onSubmit={this.handleSubmit}>
+        <Form.Item label="Note">
+          {getFieldDecorator('note', {
+            rules: [{ required: true, message: 'Please input your note!' }],
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label="Gender">
+          {getFieldDecorator('gender', {
+            rules: [{ required: true, message: 'Please select your gender!' }],
+          })(
+            <Select
+              placeholder="Select a option and change input text above"
+              onChange={this.handleSelectChange}
+            >
+              <Option value="male">male</Option>
+              <Option value="female">female</Option>
+            </Select>,
+          )}
+        </Form.Item>
+        <Form.Item wrapperCol={{ span: 12, offset: 5 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
         </div>
     )
 }
