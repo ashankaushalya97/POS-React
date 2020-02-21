90c8a87bee37951bdf6b93f3c59dc21420d74242
Kaweesha De Alwis <kaweesha12lk@gmail.com>
2020-02-21 17:22:26 +0530

    Data retrive and save in Item

--- src/component/Customer.js
+++ src/component/Customer.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { Form, Icon, Input, Button, Row,Col ,Table} from 'antd';
 import 'antd/dist/antd.css';
 // import '../component/Customer.css'
 
 function CustomerForm(props){
+
+    const btnStyle = {
+        margin:10
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -16,44 +20,108 @@ function CustomerForm(props){
 
       const { getFieldDecorator } = props.form;
 
+      const dataSource = [
+        {
+          key: '1',
+          id: 'C001',
+          name: "Mike",
+          address: '10 Downing Street',
+        },
+        {
+            key: '2',
+            id: 'C002',
+            name: "John",
+            address: '10 Downing Street',
+          },
+          {
+            key: '3',
+            id: 'C003',
+            name: "Adams",
+            address: '10 Downing Street',
+          },
+      ];
+      
+      const columns = [
+        {
+          title: 'ID',
+          dataIndex: 'id',
+          key: 'id',
+        },
+        {
+          title: 'Customer Name',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: 'Address',
+          dataIndex: 'address',
+          key: 'address',
+        },
+      ];
+
+
+
+
     return(
-        <div>
-             <Form onSubmit={handleSubmit} className="login-form">
+        <div className="col-12">
+           
+       
+      <Row type="flex" justify="space-between">
+      <Col span={12}>
+      <h2><Icon type="user-add" /> Manage Customers</h2>
+      <Form onSubmit={handleSubmit} className="login-form">
         <Form.Item>
-          {getFieldDecorator('username', {
-            rules: [{ required: true, message: 'Please input your username!' }],
+          {getFieldDecorator('id', {
+            rules: [{ required: true, message: 'Please input customer ID' }],
+          })(
+            <Input
+              prefix={<Icon type="safety-certificate" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Customer ID"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('name', {
+            rules: [{ required: true, message: 'Please input customer name' }],
           })(
             <Input
               prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="Username"
+              placeholder="Customer Name"
             />,
           )}
         </Form.Item>
         <Form.Item>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
+          {getFieldDecorator('address', {
+            rules: [{ required: true, message: 'Please input address' }],
           })(
             <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              type="password"
-              placeholder="Password"
+              prefix={<Icon type="home" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Address"
             />,
           )}
-        </Form.Item>
+        </Form.Item>   
+
         <Form.Item>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })(<Checkbox>Remember me</Checkbox>)}
-          <a className="login-form-forgot" href="">
-            Forgot password
-          </a>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
-          </Button>
-          Or <a href="">register now!</a>
+          <Row >
+            <Button style={btnStyle} type="primary" htmlType="submit" className="login-form-button">
+                Save
+            </Button>
+            <Button style={btnStyle} type="default" htmlType="submit" className="login-form-button">
+                Clear
+            </Button>
+            <Button style={btnStyle} type="danger" htmlType="submit" className="login-form-button">
+                Delete
+            </Button>
+          </Row>
         </Form.Item>
       </Form>
+      </Col>
+
+      <Col span={11}>
+        <Table dataSource={dataSource} columns={columns} />;
+      </Col>      
+
+     </Row>       
 
         </div>
     );
--- src/component/Item.js
+++ src/component/Item.js
@@ -1,154 +1,154 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import 'antd/dist/antd.css';
 import '../css/style.scss'
-import { Form, Icon, Input, Button,Row,Col, Table } from 'antd';
+import {Button, Col, Form, Icon, Input, Row, Table} from 'antd';
 
 
 function Item(props) {
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-  };
+    const [list, setList] = useState([]);
+    const axios = require('axios');
 
-  const columns = [
-    { title: 'Code', dataIndex: 'code', key: 'code' },
-    { title: 'Description', dataIndex: 'description', key: 'description' },
-    { title: 'Qty', dataIndex: 'qty', key: 'qty' },
-    { title: 'Unitprice', dataIndex: 'unitprice', key: 'unitprice' },
-    {
-      title: 'Action',
-      dataIndex: '',
-      key: 'x',
-      render: () => <a>Delete</a>,
-    },
-  ];
-  
-  const data = [
-    {
-      key: 1,
-      code: 'I001',
-      description: 'Laptop',
-      qty: 50,
-      unitprice: 145000.00,
-    },
-    {
-      key: 2,
-      code: 'I002',
-      description: 'Keyboard',
-      qty: 200,
-      unitprice: 22000.00,
-    },
-    {
-      key: 3,
-      code: 'I002',
-      description: 'Mouse',
-      qty: 100,
-      unitprice: 5000.00,
-    },
-  ];
-    const { getFieldDecorator } = props.form;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+                axios.post('http://localhost:5050/api/v1/items/', values)
+                    .then(function (response) {
+                        console.log(response)
+                    })
+                    .catch(function (error) {
+                        console.log(error)
+                    })
+            }
+        });
+    };
+    const {getFieldDecorator} = props.form;
+
+    useEffect(() => {
+        axios({
+            method: 'get',
+            url: 'http://localhost:5050/api/v1/items/',
+            // header: 'Access-Control-Allow-Origin',
+            responseType: 'json'
+        })
+            .then(function (response) {
+                setList(response.data);
+            });
+    });
+
+    const columns = [
+        {title: 'Code', dataIndex: 'code', key: 'code'},
+        {title: 'Description', dataIndex: 'description', key: 'description'},
+        {title: 'Qty', dataIndex: 'qty', key: 'qty'},
+        {title: 'UnitPrice', dataIndex: 'unitPrice', key: 'unitprice'},
+        {
+            title: 'Action',
+            dataIndex: '',
+            key: 'x',
+            render: () => <a>Delete</a>,
+        },
+    ];
 
     return (
-          <div>
+        <div>
             <Form onSubmit={handleSubmit} className="login-form">
-              <Row>
-                <Col span={24}>
-                  <h2>MANAGE ITEM</h2>
-                </Col>
-              </Row>
-              <Row type="flex" justify="space-around">
-                <Col span={11}>
-                <Form.Item>
-                  <h4>Item Code</h4>
-                  {getFieldDecorator('itenCode', {
-                    rules: [{ required: true, message: 'Please input your Item Code!' }],
-                  })(
-                    <Input
-                      prefix={<Icon type="code" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                      placeholder="Item Code"
-                    />,
-                  )}
-                </Form.Item>
-                </Col>
-                <Col span={11}>
-                <Form.Item>
-                  <h4>Item Description</h4>
-                  {getFieldDecorator('itemDescription', {
-                    rules: [{ required: true, message: 'Please input your Item Description!' }],
-                  })(
-                    <Input
-                      prefix={<Icon type="code" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                      placeholder="Item Description"
-                    />,
-                  )}
-                </Form.Item>
-                </Col>
-              </Row>
-              <Row type="flex" justify="space-around">
-                <Col span={11}>                
-                <Form.Item>
-                  <h4>Quntity</h4>
-                  {getFieldDecorator('itemQty', {
-                    rules: [{ required: true, message: 'Please input your Quntity' }],
-                  })(
-                    <Input
-                      prefix={<Icon type="code" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                      placeholder="Quntity"
-                    />,
-                  )}
-                </Form.Item></Col>
-                <Col span={11}>
-                <Form.Item>
-                  <h4>Item Unitprice</h4>
-                  {getFieldDecorator('itemUnitprice', {
-                    rules: [{ required: true, message: 'Please input your Item Unitprice!' }],
-                  })(
-                    <Input
-                      prefix={<Icon type="code" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                      placeholder="Unitprice"
-                    />,
-                  )}
-                </Form.Item>
-                </Col>
-              </Row>
-              <Row >
-                <Col span={11}>
-                  <div className="button-class">
-                    <Form.Item>
-                      <Button type="primary" htmlType="submit" className="login-form-button">
-                        Save
-                      </Button>&nbsp;&nbsp;
-                      <Button type="primary" htmlType="submit" className="login-form-button">
-                        Update
-                      </Button>&nbsp;&nbsp;
-                      <Button type="default" htmlType="button" className="login-form-button">
-                        Clear
-                      </Button>&nbsp;&nbsp;
-                      {/* <Button type="danger" htmlType="submit" className="login-form-button">
+                <Row>
+                    <Col span={24}>
+                        <h2>MANAGE ITEM</h2>
+                    </Col>
+                </Row>
+                <Row type="flex" justify="space-around">
+                    <Col span={11}>
+                        <Form.Item>
+                            <h4>Item Code</h4>
+                            {getFieldDecorator('code', {
+                                rules: [{required: true, message: 'Please input your Item Code!'}],
+                            })(
+                                <Input
+                                    prefix={<Icon type="code" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                    placeholder="Item Code"
+                                />,
+                            )}
+                        </Form.Item>
+                    </Col>
+                    <Col span={11}>
+                        <Form.Item>
+                            <h4>Item Description</h4>
+                            {getFieldDecorator('description', {
+                                rules: [{required: true, message: 'Please input your Item Description!'}],
+                            })(
+                                <Input
+                                    prefix={<Icon type="code" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                    placeholder="Item Description"
+                                />,
+                            )}
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row type="flex" justify="space-around">
+                    <Col span={11}>
+                        <Form.Item>
+                            <h4>Quntity</h4>
+                            {getFieldDecorator('qty', {
+                                rules: [{required: true, message: 'Please input your Quantity'}],
+                            })(
+                                <Input
+                                    prefix={<Icon type="code" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                    placeholder="Quantity"
+                                />,
+                            )}
+                        </Form.Item></Col>
+                    <Col span={11}>
+                        <Form.Item>
+                            <h4>Item Unitprice</h4>
+                            {getFieldDecorator('unitPrice', {
+                                rules: [{required: true, message: 'Please input your Item Unitprice!'}],
+                            })(
+                                <Input
+                                    prefix={<Icon type="code" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                    placeholder="Unitprice"
+                                />,
+                            )}
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={11}>
+                        <div className="button-class">
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" className="login-form-button">
+                                    Save
+                                </Button>&nbsp;&nbsp;
+                                <Button type="primary" htmlType="submit" className="login-form-button">
+                                    Update
+                                </Button>&nbsp;&nbsp;
+                                <Button type="default" htmlType="button" className="login-form-button">
+                                    Clear
+                                </Button>&nbsp;&nbsp;
+                                {/* <Button type="danger" htmlType="submit" className="login-form-button">
                         Delete
                       </Button> */}
-                    </Form.Item>
-                  </div>
-                </Col>
-              </Row>
-              <Row>
-                <Col span={24}>
-                <Table
-                  columns={columns}
-                  expandedRowRender={record => <p style={{ margin: 0 }}>{record.description}</p>}
-                  dataSource={data}/>
-                </Col>
-              </Row>
+                            </Form.Item>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={24}>
+                        <Table
+                            rowKey={record => record.code}
+                            columns={columns}
+                            dataSource={list}/>
 
-               
-              </Form>
+                    </Col>
+                </Row>
 
-           </div>
+
+            </Form>
+
+        </div>
     );
 }
-export default Form.create({ name: 'normal_login' })(Item);
+
+export default Form.create({name: 'normal_login'})(Item);
